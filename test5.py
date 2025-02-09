75ba30a851c35edb7a986007fc97758cca7522ac
kiruba-07 <kirubaganesan26@gmail.com>
2025-02-09 12:43:46 +0530

    Delete test5.py

--- test5.py
+++
@@ -1,9 +0,0 @@
-#Count the occurrence of each element from a list
-sample_list=[11,45,8,11,23,45,23,45,89]
-a={}
-for i in sample_list:
-    if i in a:
-        a[i]=a[i]+1
-    else:
-        a[i]=1
-print(a)
