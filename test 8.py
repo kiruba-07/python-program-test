abd5210e476f3389b1e77061dbc74c2006c84b10
kiruba-07 <kirubaganesan26@gmail.com>
2025-02-09 12:37:29 +0530

    Delete test 8.py

--- test 8.py
+++
@@ -1,8 +0,0 @@
-#Clean the datset and update the file
-import pandas as pd
-df=pd.read_csv("C:/Users/DELL/Downloads/rainfall prediction.csv",na_values={
-"pressure":["?","n.a"],
-"maxtemp":["?","n.a"],
-"cloud":["?","n.a"],
-"humidity":["?","n.a"]})
-print(df)
